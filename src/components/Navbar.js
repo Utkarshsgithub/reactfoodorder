ea5496ab20345549cd9c91b420c76d86ed64b067
Utkarshsgithub <utkarshcodes13@gmail.com>
2022-03-13 14:16:06 +0530

    meta data changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,12 @@ import { useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+
   const amount = useSelector((state) => state.amount);
+  
   return (
     <nav className="navbar">
-        <h1 className='logo'>Bhojan</h1>
+        <Link className='remove-link-style' to='/'><h1 className='logo'>Bhojan</h1></Link>
         <ul className="remove-list-style">
             <li><Link className='remove-link-style' to='/'>Home</Link></li>
             <li><Link className='remove-link-style' to='/cart'>Cart<span style={{'marginLeft':'5px', 'color':'var(--tirtary-color)'}}>${amount}</span></Link></li>
